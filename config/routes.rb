36cff91c406faaea1badd1cd47503c7045fcf63f
user-onion-japan-1919 <shuta.12.27st@gmail.com>
2025-01-15 10:59:23 +0900

    #1コントローラーの定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'messages/index'
-    root to: "messages#index"
+    root to: "items#index"
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :items # 商品関連のルーティングを設定
 end
