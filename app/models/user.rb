fceea10871938ed31e77bc28d3ce35ccb38cc5ee
user-onion-japan-1919 <shuta.12.27st@gmail.com>
2025-01-20 19:41:59 +0900

    もれ

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,10 @@ class User < ApplicationRecord
   # ニックネーム
   validates :nickname, presence: true
 
- 
-
   # パスワード (英数字混合)
-  validates :password
-                       format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'is invalid. Include both letters and numbers' }
+  validates :password,
+            format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/,
+                      message: 'is invalid. Include both letters and numbers' }
 
   # 名前 (全角文字)
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
