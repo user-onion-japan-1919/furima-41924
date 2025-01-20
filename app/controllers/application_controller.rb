721ac611cb40980b3fdd6d4ce7eb1f3e906f5d46
user-onion-japan-1919 <shuta.12.27st@gmail.com>
2025-01-20 19:01:36 +0900

    #5 エラーメッセージの位置修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,22 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth, except: [:new, :update] # 新規登録とログインを除外
+  before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up,
+      keys: [
+        :nickname,
+        :last_name,
+        :first_name,
+        :last_name_furigana,
+        :first_name_furigana,
+        :date_of_birth
+      ]
+    )
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
+  devise_for :users
+  
 
 
 
