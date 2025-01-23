6b6fb6fc7d7957e0feef2cca6469544e04f7e9ff
user-onion-japan-1919 <shuta.12.27st@gmail.com>
2025-01-23 17:30:17 +0900

    #5. 5

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    全てのアイテムを取得
+    # 全てのアイテムを取得
     # @items = Item.all
   end
 
