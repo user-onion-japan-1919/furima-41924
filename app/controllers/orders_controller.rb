0c357f48a037f8a5ac5fb7d25b7f36649fd6e045
user-onion-japan-1919 <shuta.12.27st@gmail.com>
2025-01-24 18:19:02 +0900

    ＃10リダイレクト

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,13 @@ class OrdersController < ApplicationController
 
   def new
     @item = Item.find(params[:item_id])
+
+    # 現在のユーザーが商品の出品者である場合、トップページにリダイレクト
+    redirect_to root_path, alert: '自分が出品した商品は購入できません。' if @item.user == current_user
+
+    # 商品が売却済みか確認
+    redirect_to root_path, alert: 'この商品はすでに売却済みです。' if @item.sold_out
+
     @order = Order.new
   end
 
@@ -14,7 +21,7 @@ class OrdersController < ApplicationController
 
     if @order.save
       # 購入完了処理
-      @item.update(status: 'sold') # 商品のステータスを更新（必要な場合）
+      @item.update(sold_out: true) # 商品の売却済み状態を更新
       redirect_to order_path(@order), notice: '購入が完了しました。'
     else
       render :new
