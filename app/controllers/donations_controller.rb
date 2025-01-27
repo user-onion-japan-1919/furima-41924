bc8718ed39f6af16802cedf782c591226a6fef59
user-onion-japan-1919 <shuta.12.27st@gmail.com>
2025-01-27 16:03:59 +0900

    修正

--- app/controllers/donations_controller.rb
+++
@@ -1,4 +0,0 @@
-class DonationsController < ApplicationController
-  def index
-  end
-end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,52 +1,53 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item
+  before_action :check_restrictions
 
   def new
-    @item = Item.find(params[:item_id])
-
-    # 現在のユーザーが商品の出品者である場合、トップページにリダイレクト
-    redirect_to root_path, alert: '自分が出品した商品は購入できません。' if @item.user == current_user
-
-    # 商品が売却済みか確認
-    redirect_to root_path, alert: 'この商品はすでに売却済みです。' if @item.sold_out
-
-    @order = Order.new
-  end
-
-  def index
-    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
-    @order = Order.new
+    @order_address = OrderAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
-    @order = Order.new(order_params)
-    @order.user = current_user
-    @order.item = @item
-
-    if @order.valid?
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
       pay_item
-      @order.save
-      redirect_to root_path
-
+      @order_address.save
+      redirect_to root_path, notice: '購入が完了しました。'
     else
-      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
-      render :new, index, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:price, :address, :payment_method)
+    params.require(:order_address).permit(
+      :post_code, :prefecture_id, :city_ward_town_village,
+      :street_address, :building_name, :phone_number, :token
+    ).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
   def pay_item
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
-      amount: order_params[:price],  # 商品の値段
-      card: order_params[:token],    # カードトークン
-      currency: 'jpy' # 通貨の種類（日本円）
+      amount: @item.price, # 商品の価格
+      card: order_params[:token], # カードトークン
+      currency: 'jpy'             # 通貨の種類（日本円）
     )
+  end
+
+  def set_item
+    @item = Item.find_by(id: params[:item_id])
+    return if @item
+
+    redirect_to root_path, alert: '該当する商品が見つかりませんでした。'
+  end
+
+  def check_restrictions
+    if @item.user_id == current_user.id
+      redirect_to root_path, alert: '自分が出品した商品は購入できません。'
+    elsif @item.sold_out
+      redirect_to root_path, alert: 'この商品はすでに売却済みです。'
+    end
   end
 end
