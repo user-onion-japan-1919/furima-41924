9e59ae1ec2c41ed28a41026ea7838b19651be560
user-onion-japan-1919 <shuta.12.27st@gmail.com>
2025-01-27 14:21:04 +0900

    ＃10 donationの追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   
     root to: "items#index"
-
+    root "donations#index"
  resources :items  # 必要に応じて追加
  resources :orders, only: [:index, :new, :create] # 購入履歴（index）が必要なら追加
+ resources :donations, only: :index
 end
