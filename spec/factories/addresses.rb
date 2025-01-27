1ebc5a4dfcb45f74683831761b482f65d79ae03d
user-onion-japan-1919 <shuta.12.27st@gmail.com>
2025-01-27 12:47:11 +0900

    #11. エラー表示修正

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :address do
-    
+    post_code { '123-4567' }
+    prefecture_id { 2 }
+    city_ward_town_village { '横浜市緑区' }
+    street_address { '青山1-1-1' }
+    building_name { '柳ビル103' }
+    phone_number { '09012345678' }
+    association :order
   end
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @address = FactoryBot.build(:address)
+  end
+
+  context '内容に問題がない場合' do
+    it '全ての情報が正しく入力されていれば保存できること' do
+      expect(@address).to be_valid
+    end
+  end
+
+  context '内容に問題がある場合' do
+    it 'post_codeが空では保存できないこと' do
+      @address.post_code = ''
+      @address.valid?
+      expect(@address.errors.full_messages).to include("Post code can't be blank")
+    end
+
+    it 'post_codeが「123-4567」の形式でないと保存できないこと' do
+      @address.post_code = '1234567'
+      @address.valid?
+      expect(@address.errors.full_messages).to include('Post code は「123-4567」の形式で入力してください')
+    end
+
+    it 'phone_numberが空では保存できないこと' do
+      @address.phone_number = ''
+      @address.valid?
+      expect(@address.errors.full_messages).to include("Phone number can't be blank")
+    end
+
+    it 'phone_numberが10桁または11桁でないと保存できないこと' do
+      @address.phone_number = '12345'
+      @address.valid?
+      expect(@address.errors.full_messages).to include('Phone number は10桁または11桁の数字で入力してください')
+    end
+  end
 end
