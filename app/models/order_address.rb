a39479127684eee915332a57d919ecc60b2eb995
user-onion-japan-1919 <shuta.12.27st@gmail.com>
2025-01-30 14:58:21 +0900

    ＃

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -8,7 +8,7 @@ class OrderAddress
     validates :user_id
     validates :item_id
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
-    validates :prefecture_id, numericality: { other_than: 0 }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city_ward_town_village
     validates :street_address
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
